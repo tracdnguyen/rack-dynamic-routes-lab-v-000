4cc571d73efe647d4b393484b96b057b3f58ce55
Trac Nguyen <nguyendtrac@gmail.com>
2018-06-14 05:40:53 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -3,6 +3,10 @@ class Application
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
+    search_item = Item.new
 
-    if req.path.match(/items/)
+    if req.path.match(/items/search_item)
+      resp.write search_item.price
+    else
+      resp.write "Error"
 end
