db4151913a57348aa04e267287a5c70e87cf7fb9
Trac Nguyen <nguyendtrac@gmail.com>
2018-06-14 05:51:37 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -10,7 +10,13 @@ class Application
         resp.write item.price
       end
     else
-      resp.write "Error"
+      resp.write "Item not found"
+      resp.status = 400
     end
+    else
+      resp.write "Route not found"
+      resp.status = 404
+    end
+    resp.finish
   end
 end
