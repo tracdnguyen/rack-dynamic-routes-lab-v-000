ff823be475aef6f38a43510f63d2a2fbe275a378
Trac Nguyen <nguyendtrac@gmail.com>
2018-06-14 05:55:08 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -5,11 +5,10 @@ class Application
     resp = Rack::Response.new
     req = Rack::Request.new(env)
 
-    if req.path.match (/items/)
+    if req.path.match(/items/)
       item_name = req.path.split("/items/").last
       if item = @@items.include?(item_name)
         resp.write item.price
-
       else
         resp.write "Item not found"
         resp.status = 400
