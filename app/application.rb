a1df7f7957d242ef4d12f3b747acba5e1a3c843b
Trac Nguyen <nguyendtrac@gmail.com>
2018-06-14 05:55:29 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -7,7 +7,7 @@ class Application
 
     if req.path.match(/items/)
       item_name = req.path.split("/items/").last
-      if item = @@items.include?(item_name)
+      if @@items.include?(item_name)
         resp.write item.price
       else
         resp.write "Item not found"
