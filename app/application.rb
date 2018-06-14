af54167006c122bc6588e4ff22943cff791a143f
Trac Nguyen <nguyendtrac@gmail.com>
2018-06-14 06:01:25 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -7,7 +7,7 @@ class Application
 
     if req.path.match(/items/)
       item_name = req.path.split("/items/").last
-      items = @@items.find(|i| i.name == item_name)
+      items = @@items.find{|i| i.name == item_name}
       if @@items.include? item
         resp.write items.price
       else
