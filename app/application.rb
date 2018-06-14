daa382ddb9600cc23cc1ba72cbd30de8e81def73
Trac Nguyen <nguyendtrac@gmail.com>
2018-06-14 05:59:58 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -7,8 +7,8 @@ class Application
 
     if req.path.match(/items/)
       item_name = req.path.split("/items/").last
-      if @@items.find(|i| i.name == item_name)
-        resp.write item.name
+      if item = @@items.find(|i| i.name == item_name)
+        resp.write item.price
       else
         resp.write "Item not found"
         resp.status = 400
